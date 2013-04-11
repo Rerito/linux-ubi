d2014922ad60896612f5176ded0822254d2627cb
Remi Marty <rerito2512@gmail.com>
2013-04-11 15:25:00 +0200

    [UBI] - Interval tree implementation for UBI
    This files contains an interval RB-tree
    implementation for further use in the
    key update worker. These trees will store
    the validity domain of a specific UBI volume

--- drivers/mtd/ubi/cryptokval.h
+++ drivers/mtd/ubi/cryptokval.h
@@ -14,16 +14,15 @@ struct ubi_kval_tree {
  * @node: embedded red-black node
  * @d: least element of the integer segment
  * @u: greatest element of the integer segment
- * @u_max: the maximum of the greatest elements of the
- *         node siblings (including the current node)
  */
 struct ubi_kval_node {
 	struct rb_node node;
-	u32 d, u, u_max;
+	u32 d, u;
 };
 
 int ubi_kval_init_tree(struct ubi_kval_tree *tree);
 
 int ubi_kval_insert(struct ubi_kval_tree *tree, u32 d, u32 u);
+
 
 
